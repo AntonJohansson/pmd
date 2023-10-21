2b26f5a52a4ac17d0259a6075534f2e1b1ff71be
Anton Johansson <97antjoh@gmail.com>
2023-10-21 22:24:06 +0200

    sync

--- src/net.zig
+++ src/net.zig
@@ -465,23 +465,23 @@ pub fn receiveMessagesServer(fd: std.os.socket_t) []Event {
     var events = temp_allocator.alloc(Event, 128) catch unreachable;
 
     // Timeout disconnected peers
-    const tickrate = 60; // TODO: move somewhere
-    for (&peers.buffer, 0..) |*peer, i| {
-        if (peer.state == .Disconnected)
-            continue;
-        if (peer.timeout < std.time.ns_per_s / tickrate) {
-            pushMessage(@intCast(i), packet.ConnectionTimeout{});
-            events[num_events] = Event {
-                .peer_disconnected = .{
-                    .peer_index = @intCast(i),
-                },
-            };
-            num_events += 1;
-            peer.state = .Disconnected;
-        } else {
-            peer.timeout -= std.time.ns_per_s / tickrate;
-        }
-    }
+    //const tickrate = 60; // TODO: move somewhere
+    //for (&peers.buffer, 0..) |*peer, i| {
+    //    if (peer.state == .Disconnected)
+    //        continue;
+    //    if (peer.timeout < std.time.ns_per_s / tickrate) {
+    //        pushMessage(@intCast(i), packet.ConnectionTimeout{});
+    //        events[num_events] = Event {
+    //            .peer_disconnected = .{
+    //                .peer_index = @intCast(i),
+    //            },
+    //        };
+    //        num_events += 1;
+    //        peer.state = .Disconnected;
+    //    } else {
+    //        peer.timeout -= std.time.ns_per_s / tickrate;
+    //    }
+    //}
 
     while (true) {
         const nbytes = os.recvfrom(fd, input_buffer.remainingData(), 0, @ptrCast(&their_sa), &sl) catch 0;
